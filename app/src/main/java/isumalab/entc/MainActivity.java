9faa2c5a173c16a293e8a878a93eed32778b6a5e
IsuruNuwanthilaka <isurunuwanthilaka@gmail.com>
2018-08-11 18:23:22 +0530

    Add CardView. Add SemCardView. Add launching Icon. Add ScrollView.

---
+++ app/src/main/java/isumalab/entc/MainActivity.java
@@ -0,0 +1,29 @@
+package isumalab.entc;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity{
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
