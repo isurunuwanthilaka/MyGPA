ed8a6b53fc692d0da9d8dddfc336c8e689de1149
IsuruNuwanthilaka <isurunuwanthilaka@gmail.com>
2018-10-07 22:10:11 +0530

    modified info activity

--- app/src/main/java/isumalab/entc/utils/ModuleListAdapter.java
+++ app/src/main/java/isumalab/entc/utils/ModuleListAdapter.java
@@ -3,24 +3,16 @@ package isumalab.entc.utils;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 import isumalab.entc.R;
 import isumalab.entc.activity.EditModuleActivity;
-import isumalab.entc.activity.InfoActivity;
-import isumalab.entc.activity.SemesterOneActivity;
 import isumalab.entc.entity.ModuleEntity;
-
-import static android.content.ContentValues.TAG;
 
 public class ModuleListAdapter extends RecyclerView.Adapter<ModuleListAdapter.ModuleViewHolder> {
 
@@ -43,7 +35,7 @@ public class ModuleListAdapter extends RecyclerView.Adapter<ModuleListAdapter.Mo
         private ModuleViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
-            moduleItemView = itemView.findViewById(R.id.textView);
+            moduleItemView = itemView.findViewById(R.id.news);
             moduleCode = itemView.findViewById(R.id.textViewModuleCodeEdit);
             moduleGpa = itemView.findViewById(R.id.textViewGPAEdit);
             moduleCredit = itemView.findViewById(R.id.textViewCreditEdit);
